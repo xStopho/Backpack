1050708e020d1b87d449d67f060de1af09f1e492
xStopho <xstopho@outlook.de>
2023-01-24 19:04:26 +0100

    Initial commit

---
+++ src/main/java/net/stopho/backpack/BackpackPlugin.java
@@ -0,0 +1,12 @@
+package net.stopho.backpack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BackpackPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
